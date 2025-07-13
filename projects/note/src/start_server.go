57e8c219997592269be8a2eea4f67b0c1845daae
gabriel-trinidad-ethicscode <gtrinidad@ethicscode.ai>
2025-07-13 16:48:06 -0400

    feat: start server

--- projects/note/src/start_server.go
+++ projects/note/src/start_server.go
@@ -1,11 +1,13 @@
 package infranstructure
 
 import (
+	note "note/src/note/infranstructure"
+
 	"github.com/labstack/echo"
 )
 
 func Server() {
 	e := echo.New()
-	Router(e)
+	note.Router(e)
 	e.Start("0.0.0.0:8000")
 }
